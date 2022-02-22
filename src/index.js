4168b15380e324affdefafde045cc9e5ad27310a
igorpex <igor.bogdanov@gmail.com>
2022-02-22 13:07:14 +0300

    feat: morse-table function done

--- src/index.js
+++ src/index.js
@@ -1,44 +1,71 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+
+    //split expression to separate letters
+    let letters = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        let letter = expr.slice(i, i + 10);
+        letters.push(letter);
+    }
+    console.log(letters);
+
+    //upgrade MorseTable
+    let morseTable = {};
+    for (key in MORSE_TABLE) {
+        let newKey = '';
+        for (symbol of key) {
+            let digit = symbol === '-' ? '11' : '10'
+            newKey = newKey + digit;
+        }
+        newKey = ('0000000000' + newKey).slice(-10);
+        morseTable[newKey] = MORSE_TABLE[key]
+    }
+    morseTable['**********'] = ' ';
+
+    // decode expression with upgraded MorseTable
+    let result = '';
+    for (let letter of letters) {
+        result = result + morseTable[letter];
+    }
+    return result;
 }
 
 module.exports = {
